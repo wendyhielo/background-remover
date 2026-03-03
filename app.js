410ca548f11c753ebedffb599088bdd7ebec7b60
wendyhielo <whieloes@msudenver.edu>
2026-03-03 15:18:26 -0700

    app url

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ removeBtn.addEventListener("click", async () => {
     const formData = new FormData();
     formData.append("file", file);
 
-    const response = await fetch("http://localhost:8000/remove-bg", {
+    const response = await fetch("https://https://background-remover-git-339846348292.us-west4.run.app/remove-bg", {
         method: "POST",
         body: formData
     });
