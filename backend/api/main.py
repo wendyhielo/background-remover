9d2db7bdf8e7a73e00f32518d2505508afa7a004
wendyhielo <whieloes@msudenver.edu>
2026-03-03 15:36:41 -0700

    Add CORS configuration

--- backend/api/main.py
+++ backend/api/main.py
@@ -7,6 +7,16 @@ import io
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+
+    ],
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health():
