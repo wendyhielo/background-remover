e3cb90cca991143ba0d9b7d9b6ac8806df771d0e
wendyhielo <whieloes@msudenver.edu>
2026-03-03 15:20:54 -0700

    Updated changes

--- app.js
+++ app.js
@@ -13,10 +13,17 @@ removeBtn.addEventListener("click", async () => {
     const formData = new FormData();
     formData.append("file", file);
 
-    const response = await fetch("https://https://background-remover-git-339846348292.us-west4.run.app/remove-bg", {
+    const response = await fetch("https://background-remover-git-339846348292.us-west4.run.app/remove-bg", {
         method: "POST",
         body: formData
     });
+
+    if (!response.ok) {
+        const text = await response.text();
+        console.error("API error:", response.status, text);
+        alert(`API error ${response.status}: ${text}`);
+        return;
+      }
 
     const blob = await response.blob();
     const imageUrl = URL.createObjectURL(blob);
--- backend/api/main.py
+++ backend/api/main.py
@@ -9,10 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-    ],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
